9266993af6c46cfe3727d22a29c26809a6db1e76
9180544@esmad.ipp.pt <9180544@esmad.ipp.pt>
2019-01-16 22:39:59 +0000

    correçao de texto do botao "adicionar"

--- assuntos.Designer.cs
+++ assuntos.Designer.cs
@@ -137,7 +137,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(119, 49);
             this.button1.TabIndex = 5;
-            this.button1.Text = "Adiconar";
+            this.button1.Text = "Adicionar";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
