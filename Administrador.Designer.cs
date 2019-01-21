d08a5bef9113b2ada89609ca30dc4a9a51aeadc5
9180544@esmad.ipp.pt <9180544@esmad.ipp.pt>
2019-01-21 12:11:27 +0000

    form administrador concluido

--- Administrador.Designer.cs
+++ Administrador.Designer.cs
@@ -236,6 +236,7 @@
             // 
             // pictureBox2
             // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
             this.pictureBox2.Location = new System.Drawing.Point(12, 278);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(628, 385);
