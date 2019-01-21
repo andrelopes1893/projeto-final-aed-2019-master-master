020c92eaf4c58c3112dd79b547fc6060b9388977
9180544@esmad.ipp.pt <9180544@esmad.ipp.pt>
2019-01-21 12:29:25 +0000

    form notificaçoes concluido

--- notificações.Designer.cs
+++ notificações.Designer.cs
@@ -257,6 +257,7 @@
             // pictureBox2
             // 
             this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
             this.pictureBox2.Location = new System.Drawing.Point(508, 150);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(450, 450);
