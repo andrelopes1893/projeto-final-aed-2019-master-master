2b8aba4b4e7bdc79ee5d271ed191ea9c6a32b11a
9180544@esmad.ipp.pt <9180544@esmad.ipp.pt>
2019-01-21 12:32:48 +0000

    form software concluido

--- software.Designer.cs
+++ software.Designer.cs
@@ -47,8 +47,10 @@
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label3 = new System.Windows.Forms.Label();
             this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -61,7 +63,7 @@
             this.toolStripMenuItem4});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(636, 29);
+            this.menuStrip1.Size = new System.Drawing.Size(982, 29);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -102,9 +104,9 @@
             // statusStrip1
             // 
             this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
-            this.statusStrip1.Location = new System.Drawing.Point(0, 537);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 681);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(636, 22);
+            this.statusStrip1.Size = new System.Drawing.Size(982, 22);
             this.statusStrip1.TabIndex = 1;
             this.statusStrip1.Text = "statusStrip1";
             // 
@@ -112,14 +114,14 @@
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(12, 100);
+            this.listBox1.Location = new System.Drawing.Point(12, 124);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(612, 132);
+            this.listBox1.Size = new System.Drawing.Size(443, 180);
             this.listBox1.TabIndex = 2;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(185, 303);
+            this.textBox1.Location = new System.Drawing.Point(185, 412);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(270, 36);
@@ -128,7 +130,7 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Bahnschrift Condensed", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(366, 434);
+            this.button1.Location = new System.Drawing.Point(247, 564);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(208, 80);
             this.button1.TabIndex = 5;
@@ -139,7 +141,7 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Bahnschrift Condensed", 13.8F);
-            this.button2.Location = new System.Drawing.Point(83, 434);
+            this.button2.Location = new System.Drawing.Point(12, 564);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(208, 80);
             this.button2.TabIndex = 6;
@@ -152,7 +154,7 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Bahnschrift Condensed", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(32, 303);
+            this.label1.Location = new System.Drawing.Point(32, 412);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(105, 36);
             this.label1.TabIndex = 8;
@@ -163,7 +165,7 @@
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Bahnschrift Condensed", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(32, 364);
+            this.label2.Location = new System.Drawing.Point(32, 483);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(134, 36);
             this.label2.TabIndex = 9;
@@ -186,7 +188,7 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(185, 374);
+            this.comboBox1.Location = new System.Drawing.Point(185, 495);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(270, 24);
             this.comboBox1.TabIndex = 12;
@@ -197,7 +199,7 @@
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Bahnschrift Condensed", 18F);
-            this.label3.Location = new System.Drawing.Point(32, 244);
+            this.label3.Location = new System.Drawing.Point(32, 333);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(59, 36);
             this.label3.TabIndex = 13;
@@ -207,18 +209,28 @@
             // 
             this.comboBox2.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(185, 256);
+            this.comboBox2.Location = new System.Drawing.Point(185, 345);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(270, 24);
             this.comboBox2.TabIndex = 14;
             this.comboBox2.Click += new System.EventHandler(this.comboBox2_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(488, 124);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(473, 520);
+            this.pictureBox2.TabIndex = 15;
+            this.pictureBox2.TabStop = false;
             // 
             // software
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(636, 559);
+            this.ClientSize = new System.Drawing.Size(982, 703);
+            this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.comboBox1);
@@ -240,6 +252,7 @@
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -264,5 +277,6 @@
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
