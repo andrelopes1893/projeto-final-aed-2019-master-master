8922b45f11c1c0d64b7898c5048121d030552d54
9180544@esmad.ipp.pt <9180544@esmad.ipp.pt>
2019-01-21 12:23:57 +0000

    form gestao de salas concluido

--- gestao_de_salas.Designer.cs
+++ gestao_de_salas.Designer.cs
@@ -48,16 +48,18 @@
             this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
             this.nome = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // statusStrip2
             // 
             this.statusStrip2.ImageScalingSize = new System.Drawing.Size(20, 20);
-            this.statusStrip2.Location = new System.Drawing.Point(0, 581);
+            this.statusStrip2.Location = new System.Drawing.Point(0, 681);
             this.statusStrip2.Name = "statusStrip2";
-            this.statusStrip2.Size = new System.Drawing.Size(429, 22);
+            this.statusStrip2.Size = new System.Drawing.Size(982, 22);
             this.statusStrip2.TabIndex = 1;
             this.statusStrip2.Text = "statusStrip2";
             // 
@@ -77,7 +79,7 @@
             this.listBox2.ItemHeight = 16;
             this.listBox2.Location = new System.Drawing.Point(12, 105);
             this.listBox2.Name = "listBox2";
-            this.listBox2.Size = new System.Drawing.Size(394, 324);
+            this.listBox2.Size = new System.Drawing.Size(394, 388);
             this.listBox2.TabIndex = 3;
             this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
             // 
@@ -86,7 +88,7 @@
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Bahnschrift Condensed", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(12, 444);
+            this.label2.Location = new System.Drawing.Point(12, 522);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(225, 29);
             this.label2.TabIndex = 4;
@@ -94,7 +96,7 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(243, 451);
+            this.textBox2.Location = new System.Drawing.Point(243, 529);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(163, 22);
             this.textBox2.TabIndex = 5;
@@ -103,7 +105,7 @@
             // button4
             // 
             this.button4.Font = new System.Drawing.Font("Bahnschrift Condensed", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(17, 504);
+            this.button4.Location = new System.Drawing.Point(17, 591);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(180, 50);
             this.button4.TabIndex = 6;
@@ -114,7 +116,7 @@
             // button5
             // 
             this.button5.Font = new System.Drawing.Font("Bahnschrift Condensed", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button5.Location = new System.Drawing.Point(226, 504);
+            this.button5.Location = new System.Drawing.Point(226, 591);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(180, 50);
             this.button5.TabIndex = 7;
@@ -169,7 +171,7 @@
             this.toolStripMenuItem4});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(429, 29);
+            this.menuStrip1.Size = new System.Drawing.Size(982, 29);
             this.menuStrip1.TabIndex = 9;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -215,11 +217,21 @@
             this.nome.Size = new System.Drawing.Size(23, 22);
             this.nome.TabIndex = 10;
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(449, 105);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(501, 536);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
             // gestao_de_salas
             // 
             this.BackColor = System.Drawing.Color.DodgerBlue;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(429, 603);
+            this.ClientSize = new System.Drawing.Size(982, 703);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.nome);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
@@ -237,6 +249,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -263,5 +276,6 @@
         private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
         private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem4;
         private System.Windows.Forms.TextBox nome;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
